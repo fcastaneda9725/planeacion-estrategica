1952a120052ba12718eb2f6a11e1d0f7b5de09fd
fcastaneda9725 <f.castanedatrujillo@gmail.com>
2018-11-07 23:54:05 -0600

    sequelizer install

---
+++ migrations/20181108053506-create-user.js
@@ -0,0 +1,39 @@
+'use strict';
+module.exports = {
+  up: (queryInterface, Sequelize) => {
+    return queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      id: {
+        type: Sequelize.INTEGER
+      },
+      mail: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      id_detalle_usuario: {
+        type: Sequelize.INTEGER
+      },
+      rol_de_usuario: {
+        type: Sequelize.INTEGER
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: (queryInterface, Sequelize) => {
+    return queryInterface.dropTable('Users');
+  }
+};
---
+++ models/user.js
@@ -0,0 +1,19 @@
+'use strict';
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('User', {
+    id: DataTypes.INTEGER,
+    mail: DataTypes.STRING,
+    password: DataTypes.STRING,
+    id_detalle_usuario: DataTypes.INTEGER,
+    rol_de_usuario: DataTypes.INTEGER,
+    created_at: DataTypes.DATE,
+    updated_at: DataTypes.DATE
+  }, {
+    charset: 'utf8',
+    collate: 'utf8_unicode_ci'
+  });
+  User.associate = function(models) {
+    // associations can be defined here
+  };
+  return User;
+};
