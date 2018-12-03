97c7dc83c9ed5d9fc80f1f3b900b90698af6bccc
michilinsky <36964732+michilinsky@users.noreply.github.com>
2018-12-03 12:55:58 -0600

    agregue ngFor al codigo
    integracion de angular en el codigo

--- src/app/elaboracion-pe/elaboracion-pe.component.ts
+++ src/app/elaboracion-pe/elaboracion-pe.component.ts
@@ -7,9 +7,23 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ElaboracionPeComponent implements OnInit {
 
-  constructor() { }
-
+  constructor() {
+  }
+  public Opplan = ['CECyTEM','UNAM'];
+  public Opins =['Colegio de Estudios Científicos y Tecnológicos del Estado de México'];
+  public Opcct =['15ETC0042H'];
+  public Opasig =['Álgebra'];
+  public Opsem= ['1°<br>2019-2019'];
+  public Opeje=[''];
+  public Opcom =[''];
+  public OpCc =[''];
+  public OpCe =[''];
+  public OpPe =[''];
+  public OpCga =[''];
+  public OpCd =[''];
+  public OpCp =[''];
+  public OpNtM =['one'];
+  public OpEva =['one'];
   ngOnInit() {
   }
-
 }
